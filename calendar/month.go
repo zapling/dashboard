0c2cdff111c840879cee792d4161259f79541e54
Andreas Palm <andreaspalm98@gmail.com>
2022-02-06 13:35:36 +0100

    Fixed calendar.GetMonthView function

--- calendar/month.go
+++ calendar/month.go
@@ -1,38 +1,46 @@
 package calendar
 
 import (
-	"fmt"
 	"time"
 )
 
-// GetMonth returns dates for 5 full weaks
-func GetMonth(t time.Time) string {
-	var str string
+func GetMonthView(year, month int) []time.Time {
+	var days []time.Time
 
-	firstDay := date(t.Year(), int(t.Month()), 1)
+	firstDay := date(year, month, 1)
+	lastDay := date(year, month+1, 0)
 
-	offset := int(time.Monday) - int(firstDay.Weekday())
+	// calculate how many days we need to go back to get a full week
+	startOffset := 0
+	if firstDay.Weekday() != time.Monday {
+		subtract := int(firstDay.Weekday())
+		if firstDay.Weekday() == time.Sunday {
+			subtract = 7
+		}
 
-	week := 0
+		startOffset = int(time.Monday) - subtract
+	}
+
+	// calculate how may days forward we need to go until we get a full week
+	stopDay := lastDay
+	if stopDay.Weekday() != time.Sunday {
+		daysUntilCompleteWeek := int(time.Saturday) + 1 - int(lastDay.Weekday())
+		stopDay = lastDay.AddDate(0, 0, daysUntilCompleteWeek)
+	}
+
 	for {
-		if week == 5 {
+		day := date(firstDay.Year(), int(firstDay.Month()), 1+startOffset)
+
+		if day.After(stopDay) {
 			break
 		}
 
-		day := date(firstDay.Year(), int(firstDay.Month()), 1+offset)
+		days = append(days, day)
 
-		str = fmt.Sprintf("%s %d ", str, day.Day())
-
-		// if we added a sunday we are done with the current week
-		if day.Weekday() == time.Sunday {
-			str = str + "\n"
-			week++
-		}
-
-		offset++
+		startOffset++
 	}
 
-	return str
+	return days
 }
 
 func date(year, month, day int) time.Time {
